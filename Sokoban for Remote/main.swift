1895f045244ff94173568315f95029bcdcc2fcc6
romagorn <60058363+romagorn@users.noreply.github.com>
2020-09-12 22:25:48 +0600

    Initial Commit

---
+++ Sokoban for Remote/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sokoban for Remote
+//
+//  Created by romangornostay on 9/12/20.
+//  Copyright © 2020 Roman Gornostayev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
